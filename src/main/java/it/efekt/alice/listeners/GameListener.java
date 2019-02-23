76b56916477de41b50b769a4a85bedf482d22c33
efekt <licznerski.michal@gmail.com>
2019-02-23 14:28:04 +0100

    now handling nullpointerexc on some games

--- src/main/java/it/efekt/alice/listeners/GameListener.java
+++ src/main/java/it/efekt/alice/listeners/GameListener.java
@@ -6,11 +6,13 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.user.update.UserUpdateGameEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.temporal.ChronoUnit;
 
 public class GameListener extends ListenerAdapter {
-
+    private Logger logger = LoggerFactory.getLogger(GameListener.class);
     @Override
     public void onUserUpdateGame(UserUpdateGameEvent e) {
         User user = e.getUser();
@@ -19,11 +21,15 @@ public class GameListener extends ListenerAdapter {
         if (e.getOldGame() == null || user.isBot()){
             return;
         }
+
         String gameName = e.getOldGame().getName();
         GameStats gameStats = AliceBootstrap.alice.getGameStatsManager().getGameStats(user, guild, gameName);
-        long elapsed = e.getOldGame().getTimestamps().getElapsedTime(ChronoUnit.SECONDS);
-        gameStats.addTimePlayed((int) elapsed);
-        gameStats.save();
-
+        try {
+            long elapsed = e.getOldGame().getTimestamps().getElapsedTime(ChronoUnit.SECONDS);
+            gameStats.addTimePlayed((int) elapsed);
+            gameStats.save();
+        } catch (NullPointerException exc){
+            logger.debug("Closed game does not contain ElapsedTime value, omitting..");
+        }
     }
 }
