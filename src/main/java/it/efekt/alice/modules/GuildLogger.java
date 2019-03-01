a6b2cb21c2834f95621ce95e8b00a1b128d302de
efekt <licznerski.michal@gmail.com>
2019-03-01 16:23:18 +0100

    when there is no sufficient permissions to send messages to the log channel Alice is sending a single message to the guild's owner and disables Logger

--- src/main/java/it/efekt/alice/modules/GuildLogger.java
+++ src/main/java/it/efekt/alice/modules/GuildLogger.java
@@ -3,8 +3,11 @@ package it.efekt.alice.modules;
 import it.efekt.alice.core.Alice;
 import it.efekt.alice.core.AliceBootstrap;
 import it.efekt.alice.db.GuildConfig;
+import it.efekt.alice.lang.LangCode;
+import it.efekt.alice.lang.Message;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.guild.GenericGuildEvent;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
@@ -12,6 +15,7 @@ import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.core.events.user.update.UserUpdateOnlineStatusEvent;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.time.LocalDateTime;
@@ -63,6 +67,8 @@ public class GuildLogger extends ListenerAdapter {
         log(e.getGuild(), e.getMember().getEffectiveName() + " zmienił kanał: " + e.getChannelLeft().getName() + " na: " + e.getChannelJoined().getName());
     }
 
+
+
     private boolean isLoggerSet(Guild guild){
         GuildConfig config = AliceBootstrap.alice.getGuildConfigManager().getGuildConfig(guild);
         return config.getLogChannel() != null;
@@ -75,9 +81,17 @@ public class GuildLogger extends ListenerAdapter {
     public void log(Guild guild, String message){
         if (isLoggerSet(guild)){
             TextChannel logChannel = guild.getJDA().getTextChannelById(getGuildConfig(guild).getLogChannel());
-            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
-            String dateTime = LocalDateTime.now().format(dateFormat);
-            logChannel.sendMessage("`["+dateTime + "]` " + message ).queue();
+            try {
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+                String dateTime = LocalDateTime.now().format(dateFormat);
+                logChannel.sendMessage("`[" + dateTime + "]` " + message).queue();
+            } catch(InsufficientPermissionException exc){
+                User guildOwner = guild.getOwner().getUser();
+                getGuildConfig(guild).setLogChannel(null);
+                guildOwner.openPrivateChannel().queue(privateChannel -> {
+                    privateChannel.sendMessage(Message.MODULE_LOGGER_INSUFFICIENT_PERMS.get(AliceBootstrap.alice.getLanguageManager().getLang(LangCode.valueOf(getGuildConfig(guild).getLocale())), guildOwner.getAsMention(), logChannel.getAsMention())).queue();
+                });
+            }
         }
     }
 
