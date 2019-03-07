d3ba80f880ecb2d16784c3e55df9cb922f17dc82
efekt <licznerski.michal@gmail.com>
2019-03-07 02:03:29 +0100

    fixes, increased max_to_print default value to 30

--- src/main/java/it/efekt/alice/commands/games/GameStatsCmd.java
+++ src/main/java/it/efekt/alice/commands/games/GameStatsCmd.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class GameStatsCmd extends Command {
     private final int MIN_TIME_PLAYED = 30;
-    private final int MAX_TO_PRINT = 20;
+    private final int MAX_TO_PRINT = 30;
 
     public GameStatsCmd(String alias) {
         super(alias);
@@ -34,8 +34,11 @@ public class GameStatsCmd extends Command {
                 .sorted(Comparator.comparing(Map.Entry<String,Long>::getValue).thenComparing(Map.Entry::getKey).reversed())
                 .collect(LinkedHashMap::new,(map,entry) -> map.put(entry.getKey(),entry.getValue()),LinkedHashMap::putAll);
         String output = "";
-        int i = 1;
+        int i = 0; // Begin index
+
+
         for (Map.Entry<String, Long> entry : sorted.entrySet()){
+            i++;
             String gameName = entry.getKey();
             long timePlayed = entry.getValue();
             long day = TimeUnit.MINUTES.toDays(timePlayed);
@@ -43,13 +46,12 @@ public class GameStatsCmd extends Command {
             long minutesPlayed = timePlayed - (TimeUnit.MINUTES.toHours(timePlayed) * 60);
 
             // Print only if timePlayed is larger than 30 minutes
-            if (timePlayed >= MIN_TIME_PLAYED){
+            //if (timePlayed >= MIN_TIME_PLAYED){
                 output = output.concat("**"+i+".** **" + gameName + "**: _" + day + "d " + hoursPlayed + "h " + minutesPlayed + "m " + "_\n");
-            }
+            //}
             if (i >= MAX_TO_PRINT){
                 break;
             }
-            i++;
         }
 
         EmbedBuilder embedBuilder = new EmbedBuilder();
