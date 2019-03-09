016641023e39ba6d5c4f3624edf48900e494312d
efekt <licznerski.michal@gmail.com>
2019-03-10 00:59:17 +0100

    temporarily added error event listener

--- src/main/java/it/efekt/alice/listeners/MessageListener.java
+++ src/main/java/it/efekt/alice/listeners/MessageListener.java
@@ -4,10 +4,14 @@ import it.efekt.alice.core.AliceBootstrap;
 import it.efekt.alice.db.UserStats;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.ExceptionEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageListener extends ListenerAdapter {
+    private Logger logger = LoggerFactory.getLogger(MessageListener.class);
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e){
@@ -18,6 +22,11 @@ public class MessageListener extends ListenerAdapter {
         userStats.addAndSave(1);
         //At this moment it saves info to db every time new message is received, todo make it save periodically later...
         //userStats.save();
+    }
+
+    @Override
+    public void onException(ExceptionEvent e){
+        logger.error(e.getCause().getLocalizedMessage());
     }
 
 }
