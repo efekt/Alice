00f69d3632c5d0b26902f94617ac3cdbbb241206
efekt <licznerski.michal@gmail.com>
2019-03-13 23:23:25 +0100

    cleanup

--- src/main/java/it/efekt/alice/core/Alice.java
+++ src/main/java/it/efekt/alice/core/Alice.java
@@ -28,7 +28,6 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
-
 import javax.security.auth.login.LoginException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
--- src/main/java/it/efekt/alice/core/AliceBootstrap.java
+++ src/main/java/it/efekt/alice/core/AliceBootstrap.java
@@ -7,7 +7,6 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
