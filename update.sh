039cd5779f50d5674c528f6444db62a2902e25a1
efekt <licznerski.michal@gmail.com>
2019-05-07 17:30:45 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ minute=`date +%M`
 FILENAME="alice-bot-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 screen -x -r "alice-bot-prod" -X stuff "^C"
-find backups/* -mtime +2 -exec rm {} \;
-mv $FILENAME backups/$year.$month.$day-$hour.$minute-$FILENAME
-mv jenkins/$FILENAME .
+find ./backups/* -mtime +2 -exec rm {} \;
+mv $FILENAME ./backups/$year.$month.$day-$hour.$minute-$FILENAME
+mv ./jenkins/$FILENAME .
 ./start.sh
