9cf433c857c1251813a41cf5d869a5053a2a2607
efekt <licznerski.michal@gmail.com>
2019-05-13 00:43:21 +0200

    change start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -d -m -S alice-bot-prod java -Xms512M -Xmx2G -jar alice-bot-1.0-SNAPSHOT-jar-with-dependencies.jar && echo 'Alice started.'
+screen -d -m -S alice-bot-prod java -Xms256M -Xmx4G -jar alice-bot-1.0-SNAPSHOT-jar-with-dependencies.jar && echo 'Alice started.'
