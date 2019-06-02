42c9b6f839620337e0571d9903c7db97c6b6c0fb
efekt <licznerski.michal@gmail.com>
2019-06-02 19:59:26 +0200

    change sout to logger

--- src/main/java/it/efekt/alice/commands/admin/ServersCmd.java
+++ src/main/java/it/efekt/alice/commands/admin/ServersCmd.java
@@ -4,11 +4,16 @@ import it.efekt.alice.commands.core.Command;
 import it.efekt.alice.commands.core.CommandCategory;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ServersCmd extends Command {
+    private Logger logger = LoggerFactory.getLogger(ServersCmd.class);
+
     public ServersCmd(String alias) {
         super(alias);
         setCategory(CommandCategory.BOT_ADMIN);
@@ -22,7 +27,7 @@ public class ServersCmd extends Command {
         guilds.sort(Comparator.comparing((Guild item) -> item.getMembers().size()).reversed());
 
         for (Guild guild : guilds){
-            System.out.println(guild.getName() + " : " + guild.getMembers().size());
+            logger.info(guild.getName() + " : " + guild.getMembers().size());
         }
 
         e.getTextChannel().sendMessage("List printed out into console.");
