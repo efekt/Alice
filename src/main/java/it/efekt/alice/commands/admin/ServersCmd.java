04b644a52ca3bb4d14b9bb5edfde6ca58216d615
efekt <licznerski.michal@gmail.com>
2019-06-10 00:01:50 +0200

    cleanup

--- src/main/java/it/efekt/alice/commands/admin/ServersCmd.java
+++ src/main/java/it/efekt/alice/commands/admin/ServersCmd.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
