7f071edcf843714347fdc8ab0b73843501c701e9
efekt <licznerski.michal@gmail.com>
2019-08-21 18:46:55 +0200

    change Xmx parameter in startup script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -d -m -S alice-bot-prod java -Xms1G -Xmx5G -jar alice-bot-1.0-SNAPSHOT-jar-with-dependencies.jar && echo 'Alice started.'
+screen -d -m -S alice-bot-prod java -Xms1G -Xmx3G -jar alice-bot-1.0-SNAPSHOT-jar-with-dependencies.jar && echo 'Alice started.'
