c8478f28db225776c7eedfbb27da439ad76ab23b
efekt <licznerski.michal@gmail.com>
2019-09-26 23:17:52 +0200

    add filtering to DanbooruApi, now the bot remembers last sent images and tries not to send them again

--- src/main/java/it/efekt/alice/modules/DanbooruApi.java
+++ src/main/java/it/efekt/alice/modules/DanbooruApi.java
@@ -13,12 +13,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DanbooruApi {
     private final int LIMIT = 20;
+    // HashMap<ServerId, List<PictureId>>
+    private HashMap<String, List<Integer>> lastPictures= new HashMap<>();
 
     public void sendPicture(MessageReceivedEvent event, DanbooruRating rating, String tag){
-
+        String guildId = event.getGuild().getId();
         try {
             URL url = new URL("https://danbooru.donmai.us/posts.json?random=true&limit="+LIMIT+"&tags=rating:"+rating.getName()+"%20"+tag);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -43,6 +48,7 @@ public class DanbooruApi {
             JsonArray array = new JsonParser().parse(sb.toString()).getAsJsonArray();
             String imgUrl;
             String character;
+            int imgId;
             int score;
 
             JsonObject bestRatingObject = null;
@@ -51,6 +57,11 @@ public class DanbooruApi {
                 JsonObject jsonObject = element.getAsJsonObject();
                 //Filtering results based on score, to return best quality picture possible
                 if (jsonObject.has("file_url")){
+
+                    if (containsId(guildId, jsonObject.get("id").getAsInt())){
+                        continue;
+                    }
+
                         score = jsonObject.get("score").getAsInt();
                         // if bestRatingObject doesn't exist, assign current one to it
                         if (bestRatingObject == null){
@@ -65,8 +76,16 @@ public class DanbooruApi {
                 }
             }
 
+            // if nothing has been found (applies usually to the cases when current tag's images has been all already sent)
+            if (bestRatingObject == null){
+                this.lastPictures.get(guildId).clear();
+                event.getChannel().sendMessage(Message.CMD_HENTAI_ERROR_NOT_FOUND.get(event)).queue();
+                return;
+            }
+
             imgUrl = bestRatingObject.get("file_url").getAsString();
             character = bestRatingObject.get("tag_string_character").getAsString();
+            imgId = bestRatingObject.get("id").getAsInt();
 
             if (imgUrl.isEmpty()){
                 event.getChannel().sendMessage(Message.CMD_HENTAI_ERROR_NOT_FOUND.get(event)).queue();
@@ -80,10 +99,37 @@ public class DanbooruApi {
                 embedBuilder.setFooter(character, null);
             }
 
+            // add id to last retrieved img ids
+            addId(guildId, imgId);
+
             event.getChannel().sendMessage(embedBuilder.build()).queue();
         } catch(IOException exc){
             exc.printStackTrace();
         }
 
+    }
+
+    private boolean containsId(String guildId, int pictureId){
+        if (!this.lastPictures.containsKey(guildId)){
+            return false;
+        }
+
+        if (this.lastPictures.get(guildId).contains(pictureId)){
+            return true;
+        }
+
+        return false;
+    }
+
+    private void addId(String guildId, int pictureId){
+        if (this.lastPictures.containsKey(guildId)){
+            if (!this.lastPictures.get(guildId).contains(pictureId)){
+                this.lastPictures.get(guildId).add(pictureId);
+            }
+        } else {
+            List<Integer> newList = new ArrayList<>();
+            newList.add(pictureId);
+            this.lastPictures.put(guildId, newList);
+        }
     }
 }
