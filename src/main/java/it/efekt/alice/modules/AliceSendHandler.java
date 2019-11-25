18b7e4fdc71b468f36ecb009f4f3bf4f21d21ada
efekt <licznerski.michal@gmail.com>
2019-11-25 02:26:33 +0100

    update SendHandler

--- src/main/java/it/efekt/alice/modules/AliceSendHandler.java
+++ src/main/java/it/efekt/alice/modules/AliceSendHandler.java
@@ -1,28 +1,32 @@
 package it.efekt.alice.modules;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
+import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 import java.nio.ByteBuffer;
 
 public class AliceSendHandler implements AudioSendHandler {
     private final AudioPlayer audioPlayer;
-    private AudioFrame lastFrame;
+    private MutableAudioFrame frame;
+    private ByteBuffer buffer;
 
     public AliceSendHandler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
+        this.buffer = ByteBuffer.allocate(1024);
+        this.frame = new MutableAudioFrame();
+        this.frame.setBuffer(this.buffer);
     }
 
     @Override
     public boolean canProvide() {
-        lastFrame = audioPlayer.provide();
-        return lastFrame != null;
+        return this.audioPlayer.provide(this.frame);
     }
 
     @Override
     public ByteBuffer provide20MsAudio() {
-        return ByteBuffer.wrap(lastFrame.getData());
+        this.buffer.flip();
+        return this.buffer;
     }
 
     @Override
