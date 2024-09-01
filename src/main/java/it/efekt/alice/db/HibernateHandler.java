591a103087767eb987f6bf85073020b5060f7d46
efekt <licznerski.michal@gmail.com>
2024-09-01 14:35:11 +0200

    remove GameStats entity

--- src/main/java/it/efekt/alice/db/HibernateHandler.java
+++ src/main/java/it/efekt/alice/db/HibernateHandler.java
@@ -1,7 +1,6 @@
 package it.efekt.alice.db;
 
 import it.efekt.alice.config.Config;
-import it.efekt.alice.db.model.GameStats;
 import it.efekt.alice.db.model.GuildConfig;
 import it.efekt.alice.db.model.TextChannelConfig;
 import it.efekt.alice.db.model.UserStats;
@@ -32,7 +31,6 @@ public class HibernateHandler {
                 .setProperty("hibernate.connection.password", config.getMysqlPassword())
                 .addAnnotatedClass(GuildConfig.class)
                 .addAnnotatedClass(UserStats.class)
-                .addAnnotatedClass(GameStats.class)
                 .addAnnotatedClass(TextChannelConfig.class);
         this.sessionFactory = configuration.buildSessionFactory();
     }
